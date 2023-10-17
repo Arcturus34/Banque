92297e582918cae610e227e15f02c8ed3ff4c7a3
LilianS <Lilian_Soulairol@etu.u-bourgogne.fr>
2023-10-17 14:24:35 +0200

    Test de la classe Client

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@
 int main() {
     Date date(17, 10, 2023);
     std::cout << date.toString() << std::endl;
+    Client client("Lili", "Lili","24 rue de la biscotte", date);
+    std::cout << client.toString() << std::endl;
     return 0;
 }
